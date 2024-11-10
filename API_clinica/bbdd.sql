65b530a85ed487e2c7e8ccb7f102139606d89ea8
iriavidal <iriavidal96@gmail.com>
2024-11-10 22:01:03 +0100

    Base de datos modificada

--- API_clinica/bbdd.sql
+++ API_clinica/bbdd.sql
@@ -5,7 +5,7 @@ USE gestion_clinica;
 -- Tabla Rol
 CREATE TABLE Rol (
     idRol INT PRIMARY KEY AUTO_INCREMENT,
-    nombreRol VARCHAR(50) UNIQUE
+    nombreRol VARCHAR(50) UNIQUE NOT NULL
 );
 
 -- Insertar roles iniciales
@@ -13,30 +13,30 @@ INSERT INTO Rol (nombreRol) VALUES ('cliente'), ('veterinario'), ('tecnico');
 
 -- Tabla Usuario genérica
 CREATE TABLE Usuario (
-    idUsuario INT PRIMARY KEY,
-    username VARCHAR(10)  NOT NULL,
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(10) NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     apellidos VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
-    email VARCHAR(100),
-    idRol INT,
+    email VARCHAR(100) NOT NULL,
+    idRol INT NOT NULL,
     FOREIGN KEY (idRol) REFERENCES Rol(idRol)
 );
 
 -- Tabla Cliente (relacionada con Usuario)
 CREATE TABLE Cliente (
-    idCliente INT PRIMARY KEY,
-    idUsuario INT,
+    idCliente INT PRIMARY KEY AUTO_INCREMENT,
+    idUsuario INT NOT NULL,
     dni VARCHAR(15) NOT NULL,
     direccion VARCHAR(255),
-    telefono VARCHAR(15),
+    telefono VARCHAR(15) NOT NULL,
     FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
 );
 
 -- Tabla TipoMascota para almacenar los tipos de mascotas
 CREATE TABLE TipoMascota (
     idTipo INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50) UNIQUE
+    nombre VARCHAR(50) UNIQUE NOT NULL
 );
 
 -- Insertar tipos iniciales
@@ -44,12 +44,12 @@ INSERT INTO TipoMascota (nombre) VALUES ('perro'), ('gato'), ('pájaro'), ('rept
 
 -- Tabla Mascotas
 CREATE TABLE Mascotas (
-    idMascota INT PRIMARY KEY,
-    idUsuario INT,
-    idTipo INT,
-    nombre INT NOT NULL,
+    idMascota INT PRIMARY KEY AUTO_INCREMENT,
+    idUsuario INT NOT NULL,
+    idTipo INT NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
     fecha_nac DATE,
-    nChip VARCHAR(50) NOT NULL,
+    nChip VARCHAR(50),
     raza VARCHAR(50),
     FOREIGN KEY (idUsuario) REFERENCES Cliente(idCliente),
     FOREIGN KEY (idTipo) REFERENCES TipoMascota(idTipo)
@@ -57,19 +57,19 @@ CREATE TABLE Mascotas (
 
 -- Tabla Historiales
 CREATE TABLE Historiales (
-    idHistorial INT PRIMARY KEY,
-    idMascota INT,
-    urlArchivo VARCHAR(255),
-    fecha_y_hora DATETIME,
+    idHistorial INT PRIMARY KEY AUTO_INCREMENT,
+    idMascota INT NOT NULL,
+    urlArchivo VARCHAR(255) NOT NULL,
+    fecha_y_hora DATETIME NOT NULL,
     FOREIGN KEY (idMascota) REFERENCES Mascotas(idMascota) ON DELETE CASCADE
 );
 
 -- Tabla Citas
 CREATE TABLE Citas (
-    idCita INT PRIMARY KEY,
-    idMascota INT,
-    idVeterinario INT,
-    fecha_y_hora DATETIME,
+    idCita INT PRIMARY KEY AUTO_INCREMENT,
+    idMascota INT NOT NULL,
+    idVeterinario INT NOT NULL,
+    fecha_y_hora DATETIME NOT NULL,
     FOREIGN KEY (idMascota) REFERENCES Mascotas(idMascota),
     FOREIGN KEY (idVeterinario) REFERENCES Usuario(idUsuario)
 );
