da7a74a7e1d980d29d64a63f48f5bf56ca5f0958
iriavidal <iriavidal96@gmail.com>
2024-11-10 20:01:37 +0100

    API creada y base de datos añadida

---
+++ API_clinica/config/definiciones.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Database;
+
+return [
+    Database::class => function () {
+        return new Database(
+            host: "127.0.0.1",
+            name: "gestion_clinica",
+            user: "root",
+            password: ""
+        );
+    }
+];
