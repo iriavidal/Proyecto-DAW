973892d6b710aafb0e4f54682654d3907206b8b7
iriavidal <iriavidal96@gmail.com>
2024-11-16 13:04:43 +0100

    Añadidos nuevos módulos de home al feature.module

--- gestor_clinica/src/app/features/feature.module.ts
+++ gestor_clinica/src/app/features/feature.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './home/componentes/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
+import { TituloComponent } from './home/componentes/titulo/titulo.component';
+import { ServiciosComponent } from './home/componentes/servicios/servicios.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent],
+  declarations: [HomeComponent, HeaderComponent, TituloComponent, ServiciosComponent],
   imports: [BrowserModule, MatIconModule],
   providers: [],
   bootstrap: [],
