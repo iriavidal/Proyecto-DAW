354a7b14f7edc3b8912f0155af214c27d2a3c96a
iriavidal <iriavidal96@gmail.com>
2024-11-16 14:39:59 +0100

    Terminado componente de servicio

--- gestor_clinica/src/app/features/home/componentes/servicios/servicios.component.ts
+++ gestor_clinica/src/app/features/home/componentes/servicios/servicios.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.scss']
+  styleUrls: ['./servicios.component.scss'],
 })
 export class ServiciosComponent {
-
+  cards: any[] = [
+    {
+      titulo: 'Servicio',
+      subtitulo: 'Breve descripción',
+      src: 'https://cdn.pixabay.com/photo/2019/08/05/11/58/health-4385852_1280.jpg',
+    },
+    {
+      titulo: 'Servicio',
+      subtitulo: 'Breve descripción',
+      src: 'https://cdn.pixabay.com/photo/2020/04/04/19/52/medicine-5003631_1280.jpg',
+    },
+    {
+      titulo: 'Servicio',
+      subtitulo: 'Breve descripción',
+      src: 'https://cdn.pixabay.com/photo/2016/02/10/12/53/dog-1191662_1280.jpg',
+    },
+  ];
 }
