d0f548215c7ba6190b024b7c219b9ec18ed1cd9e
iriavidal <iriavidal96@gmail.com>
2024-11-16 12:29:34 +0100

    Reorganización de los componentes de home

--- gestor_clinica/src/app/app.module.ts
+++ gestor_clinica/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './features/home/home.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { MatIconModule } from '@angular/material/icon';
+
+import { FeatureModule } from './features/feature.module';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, MatIconModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FeatureModule],
   providers: [],
   bootstrap: [AppComponent],
 })
