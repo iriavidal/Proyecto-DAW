67c4231684ac70419205040f2cc1b12242f149d8
iriavidal <iriavidal96@gmail.com>
2024-11-17 19:41:01 +0100

    Añadidos componentes de dialogs del footer

--- gestor_clinica/src/app/layout/layout.module.ts
+++ gestor_clinica/src/app/layout/layout.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { DialogPrivacidadComponent } from './footer/componentes/dialog-privacidad/dialog-privacidad.component';
+import { DialogAvisoLegalComponent } from './footer/componentes/dialog-aviso-legal/dialog-aviso-legal.component';
+import { DialogCookiesComponent } from './footer/componentes/dialog-cookies/dialog-cookies.component';
 
 @NgModule({
-  declarations: [FooterComponent],
+  declarations: [FooterComponent, DialogPrivacidadComponent, DialogAvisoLegalComponent, DialogCookiesComponent],
   imports: [CommonModule],
   exports: [FooterComponent],
 })
