1fbf988cb88d698e4e8a55224bd5fe2744e7ae06
iriavidal <iriavidal96@gmail.com>
2024-11-25 22:15:24 +0100

    Terminada maquetación del menú mascotas

--- gestor_clinica/src/app/features/mascotas/mascotas.component.ts
+++ gestor_clinica/src/app/features/mascotas/mascotas.component.ts
@@ -1,14 +1,54 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MascotasService } from 'src/app/services/datos/mascotas.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-mascotas',
   templateUrl: './mascotas.component.html',
-  styleUrl: './mascotas.component.scss',
+  styleUrls: ['./mascotas.component.scss'],
 })
-export class MascotasComponent {
-  @Input() listadoMascotas: any[] = [];
+export class MascotasComponent implements OnInit {
+  listadoMascotas: any[] = [];
+  userId: number | null = null;
+  ultimasCitas: { [key: number]: string } = {}; // Clave: id_mascota, Valor: última cita
+
+  constructor(
+    private tokenService: TokenService,
+    private mascotasService: MascotasService
+  ) {}
 
   ngOnInit(): void {
-    console.log(this.listadoMascotas);
+    this.userId = this.tokenService.getUserIdFromToken();
+
+    if (this.userId) {
+      this.mascotasService.getMascotasUser(this.userId).subscribe({
+        next: (data) => {
+          if (data.results && data.results.length > 0) {
+            this.listadoMascotas = data.results;
+
+            this.listadoMascotas.forEach((mascota) => {
+              this.mascotasService.getUltimaCita(mascota.id_mascota).subscribe({
+                next: (data) => {
+                  if (data.results && data.results.length > 0) {
+                    this.ultimasCitas[mascota.id_mascota] =
+                      data.results[0].fecha_y_hora;
+                  } else {
+                    this.ultimasCitas[mascota.id_mascota] = 'nunca';
+                  }
+                },
+                error: () => {
+                  this.ultimasCitas[mascota.id_mascota] = 'nunca';
+                },
+              });
+            });
+          } else {
+            console.error('No se encontraron mascotas para este usuario.');
+          }
+        },
+        error: (err) => console.error('Error al obtener las mascotas:', err),
+      });
+    } else {
+      console.error('No se pudo obtener el ID del usuario desde el token.');
+    }
   }
 }
