8b21e5b0e4c6ab37b08c56e21271392018685821
iriavidal <iriavidal96@gmail.com>
2024-12-06 22:53:01 +0100

    Añadidos textos a la pantalla de inicio

--- gestor_clinica/src/app/features/home/componentes/servicios/servicios.component.ts
+++ gestor_clinica/src/app/features/home/componentes/servicios/servicios.component.ts
@@ -8,18 +8,18 @@ import { Component } from '@angular/core';
 export class ServiciosComponent {
   cards: any[] = [
     {
-      titulo: 'Servicio',
-      subtitulo: 'Breve descripción',
+      titulo: 'Consulta general',
+      subtitulo: 'Revisión médica completa para tu mascota',
       src: 'https://cdn.pixabay.com/photo/2019/08/05/11/58/health-4385852_1280.jpg',
     },
     {
-      titulo: 'Servicio',
-      subtitulo: 'Breve descripción',
+      titulo: 'Vacunación',
+      subtitulo: 'Protege a tu mascota de enfermedades comunes',
       src: 'https://cdn.pixabay.com/photo/2020/04/04/19/52/medicine-5003631_1280.jpg',
     },
     {
-      titulo: 'Servicio',
-      subtitulo: 'Breve descripción',
+      titulo: 'Peluquería y estética',
+      subtitulo: 'Mantén a tu mascota limpia y saludable',
       src: 'https://cdn.pixabay.com/photo/2016/02/10/12/53/dog-1191662_1280.jpg',
     },
   ];
