faf5de28eb599e8e1dbe9ad00172db5f5eb0d978
iriavidal <iriavidal96@gmail.com>
2024-12-06 22:53:23 +0100

    Solucionado problema en el header del componente de contacto

--- gestor_clinica/src/app/features/home/componentes/contacto/contacto.component.ts
+++ gestor_clinica/src/app/features/home/componentes/contacto/contacto.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.scss'
+  styleUrl: './contacto.component.scss',
 })
 export class ContactoComponent {
+  constructor(private router: Router) {}
 
+  scrollToSection(section: string): void {
+    this.router.navigate(['/']);
+  }
 }
